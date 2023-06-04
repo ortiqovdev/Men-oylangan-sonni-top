5786a704b9565884f873a3ed6edc57db21342988
Akmal Ortiqov <116548309+ortiqovdev@users.noreply.github.com>
2023-06-04 17:28:36 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,51 @@
-﻿using System;
+jausing System;
 
 class Program
 {
     static void Main()
     {
+        Console.WriteLine("Darajali oyin. Levelni tanlang:");
+        Console.WriteLine("1. 1 - 10 oraliq");
+        Console.WriteLine("2. 1 - 100 oraliq");
+        Console.WriteLine("3. 1 - 1000 oraliq");
+        Console.Write("Tanlovni kiriting (1, 2 yoki 3): ");
+
+        int level;
+        if (!int.TryParse(Console.ReadLine(), out level)  level < 1  level > 3)
+        {
+            Console.WriteLine("Noto'g'ri tanlov! Dasturni qayta ishga tushuring.");
+            Console.ReadLine();
+            return;
+        }
+
+        int maxNumber = 0;
+        switch (level)
+        {
+            case 1:
+                maxNumber = 10;
+                break;
+            case 2:
+                maxNumber = 100;
+                break;
+            case 3:
+                maxNumber = 1000;
+                break;
+        }
+
         Random random = new Random();
-        int computerNumber = random.Next(1, 11);
+        int computerNumber = random.Next(1, maxNumber + 1);
         int guessCount = 0;
 
         while (true)
         {
-            Console.Write("Iltimos, 1 va 10 orasidagi bir son kiriting: ");
+            Console.Write($"Iltimos, 1 va {maxNumber} orasidagi bir son kiriting: ");
             int userNumber;
 
             if (int.TryParse(Console.ReadLine(), out userNumber))
             {
-                if (userNumber < 1 || userNumber > 10)
+                if (userNumber < 1 || userNumber > maxNumber)
                 {
-                    Console.WriteLine("Xato! Son 1 va 10 orasida bo'lishi kerak.");
+                    Console.WriteLine($"Xato! Son 1 va {maxNumber} orasida bo'lishi kerak.");
                     continue;
                 }
 
